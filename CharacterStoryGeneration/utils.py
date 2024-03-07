566ea790c6370dce975c8ea68f55bca79a7be87d
17000cyh <1700016603@pku.edu.cn>
2024-03-07 12:42:55 +0000

    retrieval

--- CharacterStoryGeneration/utils.py
+++ CharacterStoryGeneration/utils.py
@@ -1,34 +1,35 @@
 import time
-
+from qwen_utils import send_request
 import requests
 from openai import OpenAI
 
 # client = OpenAI(api_key="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")
 
-client = OpenAI(api_key="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")
+# client = OpenAI(api_key="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")
 
 
-def send_request(query, model="gpt-4"):
-  response = ""
+# def send_request(query, model="gpt-4"):
+#   response = ""
 
-  for i in range(0,10):
-    # try:
-      completion = client.chat.completions.create(
-        model=model,
-        messages=[
-          {"role": "user", "content": query}
-        ],
-        temperature=0.5,
-      )
+#   for i in range(0,10):
+#     # try:
+#       completion = client.chat.completions.create(
+#         model=model,
+#         messages=[
+#           {"role": "user", "content": query}
+#         ],
+#         temperature=0.5,
+#       )
 
-      # print(completion.choices[0].message)
+#       # print(completion.choices[0].message)
 
-      return completion.choices[0].message.content
-    # except:
-    #   print("sth wrong, skip")
+#       return completion.choices[0].message.content
+#     # except:
+#     #   print("sth wrong, skip")
       
-      continue
-  return response
+#       continue
+#   return response
+
 
 
 def characters_dict_to_str(characters_dict):
